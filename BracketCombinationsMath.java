a03d576fd0950012fa79ac17bf2ec6713ab526c1
Yeison Picado A <yeisonpa.96@hotmail.com>
2022-11-22 20:38:16 +0000

    Add logic to solve with math

--- BracketCombinationsMath.java
+++ BracketCombinationsMath.java
@@ -8,7 +8,6 @@ class Main {
   }
 
   public static void main (String[] args) {  
-    // keep this function call here     
     Scanner s = new Scanner(System.in);
     System.out.print(BracketCombinations(s.nextLine())); 
   }
